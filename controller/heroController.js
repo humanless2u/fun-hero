5a0167cb05da7fa6b5ac2a6f88f21a3cbf11a8ec
humanless2u <humanless2u@protonmail.com>
2022-02-08 19:04:02 -0300

    update controller

--- controller/heroController.js
+++ controller/heroController.js
@@ -1,76 +1,51 @@
 const express = require("express");
 const router = express.Router();
 
-// Fake DB
-let heroList = [
-  {
-    id: 01,
-    name: "super-dog",
-    age: 32,
-    power: {
-      attack: 60,
-      defense: 30,
-    },
-  },
-  {
-    id: 02,
-    name: "blaster",
-    age: 21,
-    power: {
-      attack: 80,
-      defense: 60,
-    },
-  },
-  {
-    id: 03,
-    name: "damanic",
-    age: 17,
-    power: {
-      attack: 700,
-      defense: 50,
-    },
-  },
-];
+const Hero = require("../model/Hero");
 
-router.get("/all", (req, res) => {
-  return res.send(heroList);
+router.get("/all", async (req, res) => {
+  let herosList = await Hero.find();
+
+  return res.send(herosList);
 });
 
-router.put("/:id", (req, res) => {
-  let id = req.params.id - 1;
+// router.put("/:name", async (req, res) => {
+//   let {
+//     name,
+//     age,
+//     power: { attack, defense },
+//   } = req.body;
+
+//   let updatedAt = Date.now();
+
+//   let updatedHero = await Hero.findOneAndUpdate(req.params.name, {
+//     name,
+//     age,
+//     power: { attack, defense },
+//     updatedAt,
+//   });
+
+//   return res.send(updatedHero);
+// });
+
+router.post("/new", async (req, res) => {
   let {
     name,
     age,
     power: { attack, defense },
   } = req.body;
 
-  heroList[id].name = name;
-  heroList[id].age = age;
-  heroList[id].power.attack = attack;
-  heroList[id].power.defense = defense;
+  let newHero = await Hero.create({ name, age, power: { attack, defense } });
 
-  return res.send(heroList[id]);
+  return res.send(newHero);
 });
 
-router.post("/new", (req, res) => {
-  let {
-    id,
-    name,
-    age,
-    power: { attack, defense },
-  } = req.body;
+// router.delete("/delete/:id", (req, res) => {
+//   let id = req.params.id - 1;
 
-  heroList.push({ id, name, age, power: { attack, defense } });
+//   heroList.splice(id);
 
-  return res.send(heroList);
-});
-
-router.delete("/delete/:id", (req, res) => {
-  let id = req.params.id - 1;
-
-  heroList.splice(id);
-
-  return res.send(heroList);
-});
+//   return res.send(heroList);
+// });
 
 module.exports = router;
